120559c69b4e3621218e140bfda659de85a9d40d
wereno4 <87480699+wereno4@users.noreply.github.com>
2022-12-05 14:51:53 +0900

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,20 +1,45 @@
-import random, sys, os
+import random, time, sys, os, keyboard
 
-global row, column, mine_count, victory_count
+global row, column, mine_count, victory_count, mine, room, mine_exploded, percentage, location, flag_count, win
 
-row: int = 0
-column: int = 0
+row: int = 10
+column: int = 10
+percentage: float = 0.2
 victory_count: int = 0
-
-if len(sys.argv) == 1:
-    row = 10
-    column = 10
-else:
-    row, column = map(int, sys.argv[1:3])
-
-mine: list[bool] =[[False for x in range(0,row)] for y in range(0,column)]
-room: list[int|str] =[[" " for x in range(0,row)] for y in range(0,column)]
+flag_count: int = 0
 mine_count: int = 0
+location: list[int] = [0,0]
+win: bool = False
+assistant: bool = False
+known: bool = False
+try:
+    for i in range(len(sys.argv)):
+        if known:
+            known = False
+            continue
+        if sys.argv[i] == "--row":
+            row = int(sys.argv[i+1])
+            known = True
+        if sys.argv[i] == "--column":
+            column = int(sys.argv[i+1])
+            known = True
+        if sys.argv[i] == "--percentage":
+            percentage = float(sys.argv[i+1]) / 100
+            known = True
+        if sys.argv[i] == "--assistant":
+            assistant = True
+except:
+    print("매개변수 오류")
+    exit()
+del known
+
+if percentage >= 1 or percentage < 0:
+    print("확률은 0% 이상 100% 미만이어야 합니다.")
+    exit()
+
+
+mine: list[bool] =[[False for x in range(0,column)] for y in range(0,row)]
+room: list[int|str] =[[" " for x in range(0,column)] for y in range(0,row)]
 
 
 def clear():
@@ -26,13 +51,22 @@ def create_mine():
     for i in range(0, row):
         for j in range(0, column):
             random.seed()
-            if random.random() <= 0.2:
+            if random.random() <= percentage:
                 mine[i][j] = True
                 mine_count += 1
+    if mine_count == row * column:
+        mine[random.randrange(0,row)][random.randrange(0,column)]  = False
+        mine_count -= 1
+    elif mine_count == 0:
+        mine[random.randrange(0,row)][random.randrange(0,column)]  = True
+        mine_count += 1
 
 
 def find_mine(x: int, y: int) -> bool:
+    if x < 0 or y < 0:
+        return False
     if mine[x][y]:
+        room[x][y] = "X"
         return True
     if room[x][y] != " ":
         return False
@@ -88,35 +122,113 @@ def find_additional_mine(x: int, y: int):
 
 
 def check_flag(x: int, y: int):
-    global victory_count
+    global victory_count, flag_count, win
+    if room[x][y] != "!" and room[x][y] != " ":
+        return
     if room[x][y] == "!":
         room[x][y] = " "
         if mine[x][y]:
             victory_count -= 1
+        flag_count -= 1
     elif room[x][y] == " ":
         room[x][y] = "!"
         if mine[x][y]:
             victory_count += 1
+        flag_count += 1
+    if victory_count == mine_count and flag_count == victory_count:
+        win = True
+
+def move(arg: str):
+    if arg == 'left':
+        if location[0] > 0:
+            location[0] -= 1
+        else:
+            location[0] = row - 1
+    if arg == 'right':
+        if location[0] < row - 1:
+            location[0] += 1
+        else:
+            location[0] = 0
+    if arg == "up":
+        if location[1] > 0:
+            location[1] -= 1
+        else:
+            location[1] = column - 1
+    if arg == "down":
+        if location[1] < column - 1:
+            location[1] += 1
+        else:
+            location[1] = 0
+
+
+def assis():
+    global assistant
+    loop_count = 0
+    while assistant:
+        if loop_count > row * column * 10:
+            assistant = False
+            break
+        starting_x: int = random.randrange(0, row)
+        starting_y: int = random.randrange(0, column)
+        if mine[starting_x][starting_y]:
+            continue
+        check: int = 0
+        for i in [starting_x-1, starting_x, starting_x+1]:
+            if i < 0 or i > row -1:
+                continue
+            for j in [starting_y-1, starting_y, starting_y+1]:
+                if j < 0 or j > column -1:
+                    continue
+                if mine[i][j]:
+                    check += 1
+        if check == 0:
+            room[starting_x][starting_y] = "0"
+            for i in [starting_x-1, starting_x, starting_x+1]:
+                if i < 0 or i > row - 1:
+                    continue
+                for j in [starting_y-1, starting_y, starting_y+1]:
+                    if j < 0 or j > column - 1:
+                        continue
+                    elif i == starting_x and j == starting_y:
+                        continue
+                    else:
+                        find_additional_mine(i,j)
+                        assistant = False
+        loop_count += 1
 
 
 create_mine()
+mine_exploded: bool = False
+if assistant:
+    assis()
 while True:
     clear()
     print("%d개의 지뢰가 있습니다." % mine_count)
+    print("깃발의 갯수: %d" % flag_count)
     for i in range(0, column):
         for j in range(0, row):
-            print("[%s]" % str(room[j][i]), end="")
+            print("[%s]" % ("A" if location[0] == j and location[1] == i and room[j][i] != "X" and not win else str(room[j][i])), end="")
         print("")
-
-    if victory_count == mine_count:
+    if win:
         print("지뢰를 모두 찾아내셨습니다!")
         exit()
 
-    x, y = map(int, input("행동할 칸의 좌표를 입력하세요\nex.)1,1\n").split(",",1))
-    action: str = input("조사할까요?(F) 아니면 깃발을 꽂을까요?(C)")[0]
-    if action == "F" or action == "f":
-        if find_mine(x-1, y-1):
-            print("지뢰가 터졌습니다.")
+    if mine_exploded:
+        print("지뢰가 터졌습니다.")
+        exit()
+    print("방향키로 이동, f로 조사, c로 깃발 꽂기, ESC로 종료.")
+    key = None
+    while key == None:
+        key = keyboard.read_key()
+        if key == 'up' or key == 'down' or key == 'left' or key == 'right':
+            move(key)
+        if key == 'f':
+            if find_mine(location[0], location[1]):
+                mine_exploded = True
+        if key == 'c':
+            check_flag(location[0], location[1])
+            time.sleep(0.1)
+        if key == 'esc':
+            print("\n안녕히가세요!")
             exit()
-    if action == "C" or action == "c":
-        check_flag(x-1, y-1)
+        time.sleep(0.1)
